fe87238fd3bdc040e86d1ac24f933e33452de808
AnnaProkhorova <avorohorp@gmail.com>
2022-04-01 21:13:37 +0300

    deploying with heroku

---
+++ starting-template/dist/precache-manifest.c46329ef87730aafd6664140cec14961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd2f84c0504989ade9c",
+    "url": "/css/app.ed68148b.css"
+  },
+  {
+    "revision": "cef40a52149af7ed406faa62dfedd63b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cd2f84c0504989ade9c",
+    "url": "/js/app.f1c45c51.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
